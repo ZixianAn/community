275c6d98951cd3b88490cd11e51c2cf12d8ba5e3
ZixianAn <zixianan@126.com>
2021-06-13 14:47:23 +0800

    社区首页开发完成

--- src/main/java/com/an/community/CommunityApplication.java
+++ src/main/java/com/an/community/CommunityApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class CommnunityApplication {
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class CommunityApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CommnunityApplication.class, args);
+		SpringApplication.run(CommunityApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/an/community/service/UserService.java
@@ -0,0 +1,18 @@
+package com.an.community.service;
+
+import com.an.community.dao.UserMapper;
+import com.an.community.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User findUserById(int id) {
+        return userMapper.selectById(id);
+    }
+
+}
