058e27c0db9341765c2d973bd277ac230ddf1dad
ZixianAn <zixianan@126.com>
2021-06-26 15:34:13 +0800

    实现发送系统通知的功能

--- src/main/java/com/an/community/controller/FollowController.java
+++ src/main/java/com/an/community/controller/FollowController.java
@@ -1,9 +1,12 @@
 package com.an.community.controller;
 
+import com.an.community.entity.Event;
 import com.an.community.entity.Page;
 import com.an.community.entity.User;
+import com.an.community.event.EventProducer;
 import com.an.community.service.FollowService;
 import com.an.community.service.UserService;
+import com.an.community.util.CommunityConstant;
 import com.an.community.util.CommunityUtil;
 import com.an.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +23,7 @@ import java.util.Map;
 import static com.an.community.util.CommunityConstant.ENTITY_TYPE_USER;
 
 @Controller
-public class FollowController {
+public class FollowController implements CommunityConstant {
 
     @Autowired
     private FollowService followService;
@@ -31,12 +34,24 @@ public class FollowController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private EventProducer eventProducer;
+
     @RequestMapping(path = "/follow", method = RequestMethod.POST)
     @ResponseBody
     public String follow(int entityType, int entityId) {
         User user = hostHolder.getUser();
 
         followService.follow(user.getId(), entityType, entityId);
+
+        // 触发关注事件
+        Event event = new Event()
+                .setTopic(TOPIC_FOLLOW)
+                .setUserId(hostHolder.getUser().getId())
+                .setEntityType(entityType)
+                .setEntityId(entityId)
+                .setEntityUserId(entityId);
+        eventProducer.fireEvent(event);
 
         return CommunityUtil.getJSONString(0, "已关注!");
     }
--- src/main/java/com/an/community/util/CommunityConstant.java
+++ src/main/java/com/an/community/util/CommunityConstant.java
@@ -2,29 +2,29 @@ package com.an.community.util;
 
 public interface CommunityConstant {
 
-    /*
-    * 激活成功
-    * */
+    /**
+     * 激活成功
+     */
     int ACTIVATION_SUCCESS = 0;
 
-    /*
-    * 重复激活
-    * */
+    /**
+     * 重复激活
+     */
     int ACTIVATION_REPEAT = 1;
 
-    /*
-    * 激活失败
-    * */
+    /**
+     * 激活失败
+     */
     int ACTIVATION_FAILURE = 2;
 
-    /*
-    * 默认状态的登录凭证的超时时间
-    * */
+    /**
+     * 默认状态的登录凭证的超时时间
+     */
     int DEFAULT_EXPIRED_SECONDS = 3600 * 12;
 
-    /*
-    * 记录状态的登录凭证超时时间
-    * */
+    /**
+     * 记住状态的登录凭证超时时间
+     */
     int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
 
     /**
@@ -41,4 +41,24 @@ public interface CommunityConstant {
      * 实体类型: 用户
      */
     int ENTITY_TYPE_USER = 3;
+
+    /**
+     * 主题: 评论
+     */
+    String TOPIC_COMMENT = "comment";
+
+    /**
+     * 主题: 点赞
+     */
+    String TOPIC_LIKE = "like";
+
+    /**
+     * 主题: 关注
+     */
+    String TOPIC_FOLLOW = "follow";
+
+    /**
+     * 系统用户ID
+     */
+    int SYSTEM_USER_ID = 1;
 }
